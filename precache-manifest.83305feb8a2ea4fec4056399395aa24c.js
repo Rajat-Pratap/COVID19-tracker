511f0d57a9d20988a0c1adb7f6dcff153ca1b7ab
Rajat-Pratap <rajat789pratap@gmail.com>
2020-05-13 14:17:33 +0530

    Updates

---
+++ precache-manifest.83305feb8a2ea4fec4056399395aa24c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8550616f5999fa4b62f80fffa67993",
+    "url": "/COVID19-tracker/index.html"
+  },
+  {
+    "revision": "ca93cfa6c5f78d34e974",
+    "url": "/COVID19-tracker/static/css/main.41f5700c.chunk.css"
+  },
+  {
+    "revision": "9674b174b81cd9fb9850",
+    "url": "/COVID19-tracker/static/js/2.0d75c0da.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID19-tracker/static/js/2.0d75c0da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca93cfa6c5f78d34e974",
+    "url": "/COVID19-tracker/static/js/main.a0d0e71c.chunk.js"
+  },
+  {
+    "revision": "096ddc70c2446eb9f55e",
+    "url": "/COVID19-tracker/static/js/runtime-main.fd73b35b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID19-tracker/static/media/image.d7265326.png"
+  }
+]);
